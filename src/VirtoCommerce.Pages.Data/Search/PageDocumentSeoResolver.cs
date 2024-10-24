3ef7674fe23c0da422c96ae9024c1e7e024a680e
Basil Kotov <basilkot@gmail.com>
2024-10-24 09:34:54 +0100

    use usergroups in searching

--- src/VirtoCommerce.Pages.Data/Search/PageDocumentSeoResolver.cs
+++ src/VirtoCommerce.Pages.Data/Search/PageDocumentSeoResolver.cs
@@ -1,15 +1,23 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using VirtoCommerce.CoreModule.Core.Seo;
+using VirtoCommerce.CustomerModule.Core.Model;
+using VirtoCommerce.CustomerModule.Core.Services;
 using VirtoCommerce.Pages.Core;
 using VirtoCommerce.Pages.Core.Models;
 using VirtoCommerce.Pages.Core.Search;
 using VirtoCommerce.Platform.Core.Common;
+using VirtoCommerce.Platform.Core.Security;
 
 namespace VirtoCommerce.Pages.Data.Search;
 
-public class PageDocumentSeoResolver(IPageDocumentSearchService searchService) : ISeoResolver
+public class PageDocumentSeoResolver(IPageDocumentSearchService searchService,
+    Func<UserManager<ApplicationUser>> userManagerFactory,
+    IMemberService memberService
+    ) : ISeoResolver
 {
     public async Task<IList<SeoInfo>> FindSeoAsync(SeoSearchCriteria criteria)
     {
@@ -34,7 +42,7 @@ public class PageDocumentSeoResolver(IPageDocumentSearchService searchService) :
             .ToList();
     }
 
-    private Task<IList<PageDocument>> FindFiles(SeoSearchCriteria criteria)
+    private async Task<IList<PageDocument>> FindFiles(SeoSearchCriteria criteria)
     {
         var permalink = criteria.Permalink?.StartsWith("/") ?? false
             ? criteria.Permalink
@@ -47,6 +55,28 @@ public class PageDocumentSeoResolver(IPageDocumentSearchService searchService) :
         searchCriteria.Skip = criteria.Skip;
         searchCriteria.Take = criteria.Take;
 
-        return searchService.SearchAllNoCloneAsync(searchCriteria);
+        var member = await FindMember(criteria.UserId);
+        if (member != null)
+        {
+            searchCriteria.UserGroups = member.Groups.ToArray();
+            searchCriteria.Visibility = PageDocumentVisibility.Private;
+        }
+
+        var result = await searchService.SearchAllNoCloneAsync(searchCriteria);
+
+        return result;
+    }
+
+    private async Task<Member> FindMember(string userId)
+    {
+        var userManager = userManagerFactory();
+        var user = userManager.Users.FirstOrDefault(x => x.Id == userId);
+
+        if (user != null)
+        {
+            return await memberService.GetByIdAsync(user.MemberId);
+        }
+
+        return null;
     }
 }
