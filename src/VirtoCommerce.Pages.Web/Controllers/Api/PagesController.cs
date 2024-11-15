d3fc1e1f5d35a0598624b98d085315be4f2015f2
Basil Kotov <basilkot@gmail.com>
2024-11-15 10:29:40 +0000

    fix criteria to search pages

--- src/VirtoCommerce.Pages.Web/Controllers/Api/PagesController.cs
+++ src/VirtoCommerce.Pages.Web/Controllers/Api/PagesController.cs
@@ -1,9 +1,9 @@
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using VirtoCommerce.CustomerModule.Core.Services;
+using VirtoCommerce.Pages.Core.Extensions;
 using VirtoCommerce.Pages.Core.Models;
 using VirtoCommerce.Pages.Core.Search;
 using VirtoCommerce.Platform.Core.Security;
@@ -20,27 +20,8 @@ public class PagesController(UserManager<ApplicationUser> userManager,
     [Route("search")]
     public async Task<ActionResult<PageDocumentSearchResult>> Search([FromBody] PageDocumentSearchCriteria criteria)
     {
-        var currentUser = await userManager.FindByNameAsync(User.Identity.Name);
-        if (currentUser != null)
-        {
-            var member = await memberService.GetByIdAsync(currentUser.MemberId);
-            if (member != null)
-            {
-                criteria.UserGroups = member.Groups.ToArray();
-            }
-
-            if (currentUser.IsAdministrator)
-            {
-                criteria.UserGroups = null;
-            }
-
-            criteria.Visibility = PageDocumentVisibility.Private;
-        }
-        else
-        {
-            criteria.UserGroups = [];
-        }
-
+        var user = await userManager.FindByNameAsync(User.Identity.Name);
+        await criteria.EnrichAuth(user, memberService);
         var result = await searchService.SearchAsync(criteria);
         return Ok(result);
     }
